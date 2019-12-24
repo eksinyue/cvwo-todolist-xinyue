9d31ab4db1a253e84ddb289b6791c2c10b8c0508
eksinyue <xinyuehehxy@gmail.com>
2019-12-24 17:09:02 +0800

    add form validation and date picker

--- app/javascript/components/TodoForm.js
+++ app/javascript/components/TodoForm.js
@@ -1,40 +1,112 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Pikaday from 'pikaday';
+import 'pikaday/css/pikaday.css';
+import { formatDate, isEmptyObject, validateTodo} from '../helpers/helpers';
 
 class TodoForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        todo: props.todo,
+        errors: {},
+      };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+
+    this.dateInput = React.createRef();
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log('submitted');
+    const { todo } = this.state;
+    const errors = validateTodo(todo);
+    if (!isEmptyObject(errors)) {
+      this.setState({ errors });
+    } else {
+      console.log(todo);
+    }
+  }
+  
+  componentDidMount() {
+    new Pikaday({
+      field: this.dateInput.current,
+      onSelect: (date) => {
+        const formattedDate = formatDate(date);
+        this.dateInput.current.value = formattedDate;
+        this.updateTodo('todo_date', formattedDate);
+      },
+    });
+  }
+
+  updateTodo(key, value) {
+    this.setState(prevState => ({
+      todo: {
+        ...prevState.todo,
+        [key]: value,
+      },
+    }));
+  }
+
+  handleInputChange(todo) {
+    const { target } = todo;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.updateTodo(name, value);
+  }
+
+  renderErrors() {
+    const { errors } = this.state;
+
+    if (isEmptyObject(errors)) {
+      return null;
+    }
+
+    return (
+      <div className="errors">
+        <h3>The following errors prohibited the todo from being saved:</h3>
+        <ul>
+          {Object.values(errors).map(error => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
         <h2>New Todo</h2>
+        {this.renderErrors()}
         <form className="todoform" onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="todo_type">
               <strong>Type:</strong>
-              <input type="text" id="todo_type" name="todo_type" />
+              <input type="text" id="todo_type" name="todo_type"
+                    onChange={this.handleInputChange} />
             </label>
           </div>
 
           <div>
             <label htmlFor="todo_date">
-              <strong>Date:</strong>
-              <input type="text" id="todo_date" name="todo_date" />
+                <strong>Date:</strong>
+                <input
+                type="text"
+                id="todo_date"
+                name="todo_date"
+                ref={this.dateInput}
+                autoComplete="off"
+                />
             </label>
           </div>
 
           <div>
             <label htmlFor="done">
               <strong>Done:</strong>
-              <input type="checkbox" id="done" name="done" />
+              <input type="checkbox" id="done" name="done" 
+                    onChange={this.handleInputChange}/>
             </label>
           </div>
           <div className="form-actions">
@@ -45,5 +117,17 @@ class TodoForm extends React.Component {
     );
   }
 }
+
+TodoForm.propTypes = {
+    todo: PropTypes.shape(),
+  };
+  
+  TodoForm.defaultProps = {
+    todo: {
+      todo_type: '',
+      todo_date: '',
+      done: false,
+    },
+  };
 
 export default TodoForm;
---
+++ app/javascript/helpers/helpers.js
@@ -0,0 +1,19 @@
+export const formatDate = (d) => {
+    const YYYY = d.getFullYear();
+    const MM = `0${d.getMonth() + 1}`.slice(-2);
+    const DD = `0${d.getDate()}`.slice(-2);
+  
+    return `${YYYY}-${MM}-${DD}`;
+  };
+
+  export const isEmptyObject = obj => Object.keys(obj).length === 0;
+
+  export const validateTodo = (todo) => {
+    const errors = {};
+  
+    if (todo.todo_type === '') {
+      errors.todo_type = 'You must enter a todo type';
+    }
+  
+    return errors;
+  }
